2b5c34b199f5299146906bb8db4620849534af5e
Miki <jasdimiklos.net@gmail.com>
2019-12-20 23:25:34 +0100

    Add axios module for blog post retrieval
    - Store Firebase project URL in REACT_APP_FIREBASE environment variable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import styles from './Blog.module.css';
+import axios from './../../utils/axios-blog';
 
-const blog = (props) => (
-  <div className={styles.Test}>This is going to be the Blog section.</div>
-)
+const Blog = (props) => {
 
-export default blog;
+  const [posts, setPosts] = useState({posts: []});
+
+  useEffect(() => {
+    axios.get('/posts.json')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  let postElements = [];
+
+  if (posts.length > 0) {
+    postElements = posts.map((post, idx) => {
+      return <p key={idx}>{post.title}</p>
+    });
+  }
+
+  return (
+    <div>
+      <div className={styles.Test}>{postElements}</div>
+    </div>
+  )
+}
+
+export default Blog;
