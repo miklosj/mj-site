7feed5cfed37a2e3728a978f12c9f984dba2be75
Miki <jasdimiklos.net@gmail.com>
2020-04-12 23:13:25 +0200

    Add loading animation to post images

--- src/components/Post/PostImage/PostImage.js
+++ src/components/Post/PostImage/PostImage.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './PostImage.module.css';
 
-const PostImage = (props) => (
-  <div className={styles.PostImageContainer}>
-    <a href={props.src}>
-      <img src={props.src} alt={props.alt} className={styles.PostImage}/>
-    </a>
-    <p className={styles.PostImageDescription}>{props.alt}</p>
-  </div>
-);
+import Loading from '../../UI/Loading/Loading';
+
+const PostImage = (props) => {
+
+  const [loaded, setLoaded] = useState(false);
+
+  let placeholder = null;
+
+  if (!loaded)
+    placeholder = (
+      <div className={styles.Placeholder}>
+        <Loading/>
+      </div>
+    );
+
+  return (
+    <div className={styles.PostImageContainer}>
+      <a href={props.src}>
+        <img          
+          src={props.src}
+          alt={props.alt}
+          className={loaded ? styles.PostImage : styles.Disabled}
+          onLoad={() => setLoaded(true)}/>
+        {placeholder}
+      </a>
+      <p className={styles.PostImageDescription}>{props.alt}</p>
+    </div>
+  );
+};
 
 export default PostImage;
---
+++ src/components/UI/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Loading.module.css';
+
+const loading = (props) => (
+  <div class={styles.Loading}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default loading;
